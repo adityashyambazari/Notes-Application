e4bc06c77d192d1f8dbed32366ad5bb6da8d4bda
excalibur <aditya.bazari6@gmail.com>
2017-11-02 23:48:20 +0530

    Initial commit

---
+++ app/src/main/java/com/wingfotech/myapplication/EditYourNote.java
@@ -0,0 +1,90 @@
+package com.wingfotech.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.util.HashSet;
+
+
+public class EditYourNote extends Activity implements TextWatcher {
+    int noteId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_your_note);
+
+        EditText editText = (EditText)findViewById(R.id.editText);
+        Intent i = getIntent();
+        noteId = i.getIntExtra("noteId", -1);
+
+        if (noteId != -1) {
+
+            editText.setText(MainActivity.notes.get(noteId));
+
+
+        }
+
+        editText.addTextChangedListener( this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edit_your_note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        MainActivity.notes.set(noteId,String.valueOf(s));
+        MainActivity.arrayAdapter.notifyDataSetChanged();
+
+        if(MainActivity.set==null)
+            MainActivity.set = new HashSet<String>();
+        else
+            MainActivity.set.clear();
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.wingfotech.myapplication", Context.MODE_PRIVATE);
+
+        MainActivity.set.addAll(MainActivity.notes);
+        sharedPreferences.edit().remove("Notes").apply();
+        sharedPreferences.edit().putStringSet("Notes", MainActivity.set).apply();
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
